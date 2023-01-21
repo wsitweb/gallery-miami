f56a1324bd8c196d10331561300f28d7d29732de
ROMAN <poman207575@gmail.com>
2023-01-21 03:49:21 +0200

    dev

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const sliderMain = new Swiper('.slider_main', {
+    freeMode: false,
+    centeredSlides: true,
+    mousewheel: true,
+    parallax: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 2.5,
+            spaceBetween: 20,
+        },
+        680: {
+            slidesPerView: 3.5,
+            spaceBetween: 60,
+        },
+    },
+});
+const sliderBg = new Swiper('.slider_bg', {
+    centeredSlides: true,
+    spaceBetween: 60,
+    slidesPerView: 3.5,
+});
+sliderMain.controller.control = sliderBg;
